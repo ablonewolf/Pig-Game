c98e6c46daad56b3346a42757349695af5e13254
ab <arkabhuiyan66@gmail.com>
2022-01-17 19:44:51 +0600

    Initializing conditons and elements

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-'use strict';
+"use strict";
+// selecting elements
+const player1score = document.getElementById("score--0");
+const player2score = document.getElementById("score--1");
+const DiceEL = document.querySelector(".dice");
+
+// initializing conditions
+player1score.textContent = 0;
+player2score.textContent = 0;
+DiceEL.classList.add("hidden");
